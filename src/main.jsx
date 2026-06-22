e403a4289712ba6e7707ff6ea8e331898d672c8b
ismail-maalim <mihnaz682@gmail.com>
2026-06-22 04:12:07 -0400

    Task 5: Configure Redux store and Provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
+/*import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
+//import store from './store.js'
+//ReactDOM.createRoot(document.getElementById('root')).render(
+  //<React.StrictMode>
+    //<Provider store={store}>
+    //<App />
+    //</Provider>
+  //</React.StrictMode>,
+//)*/
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux' // Kept only one import
+import App from './App.jsx'
+import './index.css'
 import store from './store.js'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
   </React.StrictMode>,
 )
